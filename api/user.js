19c4b86e35bb4531fbb595ef3afc20adf868ba8a
Nawapong Sitaruno <nawaphong.sit@dome.tu.ac.th>
2020-11-07 13:25:52 +0700

    release

--- api/user.js
+++ api/user.js
@@ -1,5 +1,7 @@
 let router = require('express').Router()
 let https = require('https')
+
+const token = "";
 router.post("/api/user/identify",(req, res) => {
     let user_information = []
         let username = req.body.username
@@ -11,7 +13,7 @@ router.post("/api/user/identify",(req, res) => {
             'path': '/api/v1/auth/Ad/verify',
             'headers': {
               'Content-Type': 'application/json',
-              'Application-Key': 'TUb137bfafbaf4a6f5971dcbd060cffee36222351a62a97089c1932e41279778b9f4219202d623edad7df0e3156ff22726'
+              'Application-Key': token
             }    
         }
 
@@ -37,44 +39,5 @@ router.post("/api/user/identify",(req, res) => {
         
         req_api.end();
 })
-
-// router.route("/api/user/identify")
-//     .post( (req,res)=>{
-//         let user_information = []
-//         let username = req.body.username
-//         let password = req.body.password
-
-//         let options = {
-//             'method': 'POST',
-//             'hostname': 'restapi.tu.ac.th',
-//             'path': '/api/v1/auth/Ad/verify',
-//             'headers': {
-//               'Content-Type': 'application/json',
-//               'Application-Key': 'TUb137bfafbaf4a6f5971dcbd060cffee36222351a62a97089c1932e41279778b9f4219202d623edad7df0e3156ff22726'
-//             }    
-//         }
-
-//         let req_api =  https.request(options,(res_api)=>{
-//             let chunks = [];
-//             res_api.on("data", (chunk)=>{
-//                 chunks.push(chunk)
-//             })
-//             res_api.on("end",async (chunk)=>{
-//                 var body =  Buffer.concat(chunks)
-//                 console.log(body.toString())
-//                 user_information = await JSON.parse(body.toString())
-//                 res.json(user_information)
-//             })
-//             res_api.on("error",(error)=>{
-//                 console.log(error)
-//             })
-//         })
-        
-//         var postData = `{\n\t\"UserName\":\"${username}\",\n\t\"PassWord\":\"${password}\"\n}`
-        
-//         req_api.write(postData)
-        
-//         req_api.end();
-//     })
 
 module.exports = router
--- index.js
+++ index.js
@@ -12,16 +12,10 @@ const HOST = '0.0.0.0'
 const PORT = process.env.PORT || 5000
 app.use(cors())
 app.use(express.json())
-// app.use("/api", bodyParser.json(), router)
-// app.use("/api",bodyParser.urlencoded({ extended : false}), router)
 
 app.use(require("./api/user"))
 app.use(require("./api/enroll"))
-// router.route("/enroll/pushEnrollRule")
-//     .post(async(req,res)=> {
-//         let enroll_rule = new enrollRule(req.body.text);
-//         await enroll_rule.save()
-//     })
+
 
     app.listen(PORT, HOST, () => {
         console.log("Server is running on port :", PORT)
