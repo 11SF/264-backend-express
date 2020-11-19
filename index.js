26e69534b7d2ee046d20d38e1ad0869d83fe514e
Nawapong Sitaruno <nawaphong.sit@dome.tu.ac.th>
2020-11-20 01:42:35 +0700

    add getFormbyID

--- index.js
+++ index.js
@@ -12,6 +12,12 @@ const HOST = '0.0.0.0'
 const PORT = process.env.PORT || 5000
 app.use(cors())
 app.use(express.json())
+// router.get('/', function(req, res) {
+//     res.setHeader('Access-Control-Allow-Origin', '*');
+//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
+//     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+//     res.setHeader('Access-Control-Allow-Credentials', true); 
+// });
 
 app.use(require("./api/user"))
 app.use(require("./api/enroll"))
